bb9b17a473694304d14680d9df9dbfe5e1284005
Chanjong Baek <readandaction@gmail.com>
2020-07-08 18:09:08 +0900

    Set routers

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
 
@@ -14,6 +17,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 export default app;
