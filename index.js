594709dd1fc90cac48d0664ea90fc8c1a7a238e4
Chanjong Baek <readandaction@gmail.com>
2020-07-08 15:44:16 +0900

    Set babel, nodemon

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
-PORT = 4000;
+const PORT = 4000;
+
 function handleListening() {
   console.log(`Listening on : "http://localhost:${PORT}`);
 }
+const handleHome = (req, res) => res.send("hom");
+
+app.get("/", handleHome);
 app.listen(4000, handleListening);
