2c4686f0d456a1434133e65537f11d4ab43db0f5
Chanjong Baek <readandaction@gmail.com>
2020-07-08 17:49:39 +0900

    Create app.js to export default app

--- app.js
+++ app.js
@@ -3,18 +3,17 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+
 const app = express();
 
-const PORT = 4000;
-
-function handleListening() {
-  console.log(`Listening on : "http://localhost:${PORT}`);
-}
 const handleHome = (req, res) => res.send("hom");
+
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
+
 app.get("/", handleHome);
-app.listen(4000, handleListening);
+
+export default app;
