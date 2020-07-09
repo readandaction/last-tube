26fb48977af9a4af6809a3103ecabc2ed673c225
Chanjong Baek <readandaction@gmail.com>
2020-07-09 20:01:44 +0900

    Set Controllers

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,17 @@
 import express from "express";
 import routes from "../routes";
+import { home } from "../controllers/videoControllers";
+import {
+  editProfile,
+  userDetail,
+  changePassword,
+} from "../controllers/userControllers";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.home, (req, res) => res.send("users"));
-userRouter.get(routes.editProfile, (req, res) => res.send("editProfile"));
-userRouter.get(routes.userDetail, (req, res) => res.send("userDetail"));
-userRouter.get(routes.changePassword, (req, res) => res.send("changePassword"));
+userRouter.get(routes.home, home);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
