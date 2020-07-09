deed4dd4b7b3cccb77d11bd8f2d93a9c9db87dca
Chanjong Baek <readandaction@gmail.com>
2020-07-09 19:04:22 +0900

    Set routes

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
-
-const handleHome = (req, res) => res.send("hom");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -17,8 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
