97e7d76ba6208a9bee251cf640a48a7f825cc062
Chanjong Baek <readandaction@gmail.com>
2020-07-10 15:36:40 +0900

    Install pug

--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -1,4 +1,4 @@
-export const home = (req, res) => res.send("home");
+export const home = (req, res) => res.render("home");
 export const videos = (req, res) => res.send("videos");
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("upload");
