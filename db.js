23adb94b49599504872c9bde7dbe5c1e0be6bbf3
Chanjong Baek <readandaction@gmail.com>
2020-07-14 19:49:15 +0900

    Create fake DB

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+    {
+        id=123123,
+        title="Fake Title",
+        description="Fake description",
+        views=24,
+        file="",
+        creator={
+            id=123,
+            name="fake",
+            email="dkdk@ddd.co"
+        }
+    },
+    {
+        id=123113,
+        title="Fake Title2",
+        description="Fake description3",
+        views=24,
+        file="",
+        creator={
+            id=123,
+            name="fake",
+            email="dkdk@ddd.co"
+        }
+    },
+    {
+        id=12312343,
+        title="Fake Title3",
+        description="Fake description2",
+        views=24,
+        file="",
+        creator={
+            id=123,
+            name="fake",
+            email="dkdk@ddd.co"
+        }
+    }
+]
