d1aec2cf7ceb85f8e0cc215f6c71283db7f6fc7a
Chanjong Baek <readandaction@gmail.com>
2020-07-15 14:59:56 +0900

    Add res.locals.user, Update header

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -15,7 +15,10 @@ export const postJoin = (req, res) => {
   }
 };
 
-export const login = (req, res) => res.render("login", { pageTitle: "login" });
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "login" });
+export const postLogin = (req, res) => res.redirect(routes.home);
+
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "logout" });
 export const users = (req, res) => res.render("users", { pageTitle: "users" });
--- middlewares.js
+++ middlewares.js
@@ -3,5 +3,9 @@ import routes from "./routes";
 export const localsMiddlware = (req, res, next) => {
   res.locals.siteTitle = "Last-tube";
   res.locals.routes = routes;
+  res.locals.user = {
+    isAuthenticated: true,
+    id: 1,
+  };
   next();
 };
