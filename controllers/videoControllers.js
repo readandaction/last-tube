cf1322b95b45d91ef7d8f144ff7e7de1ea37a53f
Chanjong Baek <readandaction@gmail.com>
2020-07-15 12:39:54 +0900

    Update join controller

--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -1,13 +1,19 @@
 import { videos } from "../db";
+
 export const home = (req, res) =>
   res.render("home", { pageTitle: "home", videos });
+
 export const search = (req, res) => {
   const {
     query: { term: searchingBy },
   } = req;
-
-  res.render("search", { pageTitle: "search", searchingBy: searchingBy });
+  res.render("search", {
+    pageTitle: "search",
+    searchingBy: searchingBy,
+    videos,
+  });
 };
+
 export const upload = (req, res) => res.send("upload", { pageTitle: "upload" });
 export const videoDetail = (req, res) =>
   res.send("videoDetail", { pageTitle: "videoDetail" });
