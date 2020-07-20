3fd7b9d1fb436599a0a19b4ed67eceb5ade340e7
Chanjong Baek <readandaction@gmail.com>
2020-07-21 07:37:17 +0900

    Install Eslint

--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -82,6 +82,8 @@ export const deleteVideo = async (req, res) => {
   } = req;
   try {
     await Video.findOneAndRemove({ _id: id });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
   res.redirect(routes.home);
 };
