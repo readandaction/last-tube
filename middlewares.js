b209c4d15a00972cb3869c62d8d0503dea1566b3
Chanjong Baek <readandaction@gmail.com>
2020-07-22 08:00:45 +0900

    5.1 Style footer, form

--- middlewares.js
+++ middlewares.js
@@ -7,7 +7,7 @@ export const localsMiddlware = (req, res, next) => {
   res.locals.siteTitle = "Last-tube";
   res.locals.routes = routes;
   res.locals.user = {
-    isAuthenticated: true,
+    isAuthenticated: false,
     id: 1,
   };
   next();
