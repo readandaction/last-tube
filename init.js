d3588525d5c709b8fb5816c1824412af86e249aa
Chanjong Baek <readandaction@gmail.com>
2020-07-22 11:36:09 +0900

    Set passport-local

--- init.js
+++ init.js
@@ -1,10 +1,10 @@
 import "./db";
 import app from "./app";
 import dotenv from "dotenv";
+dotenv.config();
 import "./models/Video";
 import "./models/Comment";
-
-dotenv.config();
+import "./models/User";
 
 const PORT = process.env.PORT || 4000;
 
